e33770bd4a046c8aac82bb977114e386197f7e7e
Walter <walterrjr.86@gmail.com>
2022-06-28 21:13:43 -0300

    aula 3-ok

--- 1_instalacao_vue/src/app.js
+++ 1_instalacao_vue/src/app.js
@@ -1,11 +1,23 @@
 const MyNameApp = {
     data(){
         return{
-            name: "Walter",
-            idade: 36,
-            cidade: "Curitiba",
+            /* name: "",
+            idade: "",
+            cidade: "", */
+            input_name: "",
+            input_idade: "",
+            input_cidade: ""
         }
-    }
+    },
+    methods: {
+        submitForm(e){
+            e.preventDefault();
+            console.log(this.input_name);
+            this.name = this.input_name;
+            this.idade = this.input_idade;
+            this.cidade = this.input_cidade;
+        }
+    },
 }
 
 Vue.createApp(MyNameApp).mount("#app");
