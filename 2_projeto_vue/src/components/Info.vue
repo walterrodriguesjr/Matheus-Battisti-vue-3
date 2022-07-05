b48ddbfae3adc25bde8708b4e4bb2ae9bb3e312f
Walter <you@example.com>
2022-07-05 12:27:32 -0300

    aula-9-ok

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
         <li>JavaScript</li>
         <li>PHP</li>
         <li>Python</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{email}}</p>
     </div>
 </template>
 
@@ -15,7 +17,9 @@ export default {
    name: 'Info',
    data(){
     return {
-        
+        esta_trabalhando: true,
+        mostrar_email: true,
+        email: 'walterrjr.86@gmail.com'
     }
    }
 }
