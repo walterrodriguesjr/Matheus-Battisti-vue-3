2d68caa08056d837d1e873485cd47fc228a95f31
Walter <you@example.com>
2022-07-18 07:30:00 -0300

    aula-24-ok

--- 3_projeto_make_your_burger/src/views/Home.vue
+++ 3_projeto_make_your_burger/src/views/Home.vue
@@ -2,16 +2,18 @@
   <Banner />
   <div class="main-container">
     <h1>Monte o seu burger:</h1>
-    <p>formulário</p>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue";
+import BurgerForm from "../components/BurgerForm.vue";
 export default {
   name: "Home",
   components: {
-    Banner
+    Banner,
+    BurgerForm
   }
 }
 </script>
